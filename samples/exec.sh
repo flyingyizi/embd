407b873ea99e6b8dbb10c6146d32fb222eee0a90
flyingyizi <flyingyizi@gmail.com>
2017-06-02 19:58:59 +0800

    1

---
+++ samples/exec.sh
@@ -0,0 +1,13 @@
+#!/bin/ksh
+#
+             
+USAGE="Usage:a.sh filename"
+if [ $# -ne 1 ]
+then 
+  printf "$USAGE\n"
+  exit 1
+fi
+
+GOOS=linux GOARCH=arm  go build -o execa  $1
+
+scp execa pi@192.168.1.9:~
